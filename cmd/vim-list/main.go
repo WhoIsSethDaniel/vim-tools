93c323bd0c29dbcbb50bca2c20e8a3080001d7b5
Seth Daniel <seth@sethdaniel.org>
2021-10-14 21:15:11 -0500

    add -u for printing repo url

--- cmd/vim-list/main.go
+++ cmd/vim-list/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,6 +9,10 @@ import (
 )
 
 func main() {
+	var listURL bool
+	flag.BoolVar(&listURL, "u", false, "List the repo URL along with the name")
+	flag.Parse()
+
 	plugins, err := tools.Read()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to read all plugin file: %s\n", err)
@@ -17,8 +22,13 @@ func main() {
 
 	filterAndPrint := func(f func(p tools.Plugin) bool) {
 		for _, name := range pluginNames {
-			if f(plugins[name]) {
-				fmt.Println(name)
+			plugin := plugins[name]
+			if f(plugin) {
+				fmt.Print(name)
+				if listURL {
+					fmt.Printf(" [%s]", plugin.URL)
+				}
+				fmt.Println()
 			}
 		}
 	}
