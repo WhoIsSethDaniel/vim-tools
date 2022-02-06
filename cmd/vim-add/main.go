679b97142654bcdcab04181dcabcbb23935e9352
Seth Daniel <seth@sethdaniel.org>
2022-02-05 22:15:54 -0600

    add timeout for git op

--- cmd/vim-add/main.go
+++ cmd/vim-add/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	tools "github.com/WhoIsSethDaniel/vim-tools"
 )
@@ -20,14 +22,18 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Failed to read plugins file: %s\n", err)
 		os.Exit(1)
 	}
+	baseCtx := context.Background()
 	for _, arg := range os.Args[1:] {
 		fmt.Print(" - cloning\n")
-		git := exec.Command("git", "clone", arg)
+
+		ctx, cancel := context.WithTimeout(baseCtx, 30*time.Second)
+		git := exec.CommandContext(ctx, "git", "clone", arg)
 		git.Dir = tools.PluginDir()
 		if _, err := git.Output(); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to run git command: %s\n", err)
 			os.Exit(1)
 		}
+		cancel()
 
 		plugin := plugins.Add(arg)
 
