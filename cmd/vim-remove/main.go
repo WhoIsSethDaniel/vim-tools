b71a0943eece3b038c05e0b81513000952404527
Seth Daniel <seth@sethdaniel.org>
2023-07-28 16:18:19 -0500

    add options to vim-remove

--- cmd/vim-remove/main.go
+++ cmd/vim-remove/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,36 +10,50 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 0 {
-		fmt.Fprintf(os.Stderr, "Usage: %s plugin [plugin ...]\n", filepath.Base(os.Args[0]))
+	var keep, noRemove bool
+	flag.BoolVar(&keep, "k", false, "Remove the directory but keep the plugin registered")
+	flag.BoolVar(&noRemove, "u", false, "Do not remove the directory but unregister the plugin")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-ku] plugin [plugin ...]\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
 
-	plugins, _ := tools.Read()
-	for _, arg := range os.Args[1:] {
+	plugins, err := tools.Read()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read plugins file: %s\n", err)
+		os.Exit(1)
+	}
+
+	for _, arg := range flag.Args() {
 		plugin, ok := plugins[arg]
 		if !ok {
 			fmt.Fprintf(os.Stderr, "No such plugin: %s\n", arg)
 			os.Exit(1)
 		}
-		pluginDir := filepath.Join(tools.PluginDir(), arg)
-		fi, err := os.Stat(pluginDir)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "plugin '%s' directory does not exist: %s\n", arg, err)
-			os.Exit(1)
-		}
-		if !fi.IsDir() {
-			fmt.Fprintf(os.Stderr, "Plugin '%s' has no directory: %s\n", arg, err)
-			os.Exit(1)
+		if !noRemove {
+			pluginDir := filepath.Join(tools.PluginDir(), arg)
+			fi, err := os.Stat(pluginDir)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "plugin '%s' directory does not exist: %s\n", arg, err)
+				os.Exit(1)
+			}
+			if !fi.IsDir() {
+				fmt.Fprintf(os.Stderr, "Plugin '%s' has no directory: %s\n", arg, err)
+				os.Exit(1)
+			}
+
+			fmt.Print(" - removing directory\n")
+			if err := os.RemoveAll(pluginDir); err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to remove plugin %s dir: %s\n", arg, err)
+				os.Exit(1)
+			}
 		}
 
-		fmt.Print(" - removing directory\n")
-		if err := os.RemoveAll(pluginDir); err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to remove plugin %s dir: %s\n", arg, err)
-			os.Exit(1)
+		if !keep {
+			plugins.Remove(plugin)
 		}
-
-		plugins.Remove(plugin)
 	}
 
 	fmt.Print(" - rewrite files\n")
